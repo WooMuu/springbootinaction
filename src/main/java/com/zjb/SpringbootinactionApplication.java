2bbdd723528f02c633285215182a003a97f24992
zjb <fillixzhang@163.com>
2019-11-18 16:50:26 +0800

    Initial commit

---
+++ src/main/java/com/zjb/SpringbootinactionApplication.java
@@ -0,0 +1,13 @@
+package com.zjb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootinactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootinactionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zjb/entity/Book.java
@@ -0,0 +1,76 @@
+package com.zjb.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by zjb on 2019/11/18.
+ */
+@Entity
+public class Book {
+    private long id;
+    private String reader;
+    private String isbn;
+    private String title;
+    private String author;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getReader() {
+        return reader;
+    }
+
+    public void setReader(String reader) {
+        this.reader = reader;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", reader='" + reader + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
